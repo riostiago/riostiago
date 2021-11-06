062134e4d8cda74f0630fe3c9e2c0b59cd3c80f9
Tiago Oliveira Rios <90370300+riostiago@users.noreply.github.com>
2021-11-06 20:11:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+//tipos primitivos
+
+//boolean
+var vOuF = false;
+console.log(typeof(vOuF));
+
+//number
+var numeroQualquer = 1;
+console.log(tyeof(numeroQualquer));
+
+//string
+var nome = tiago
+console.log(typeof(numeroQualquer));
+
+// como declarar
+/*var variavel = 'tiago';
+variavel = 'rios';
+console.log(variavel); */
+
+ /*let variavel2 = 'tiago';
+ variavel2 = 'rios';
+ console.log(variavel2); */
+
+ /*const constante = 'tiago';
+ constante = 'oliveira';
+ console.log(constante)*/
+
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopolocalInterno = 'local';
+    console.log(escopolocalInterno);
+}
+
+escopoLocal(); */
+    
+// atribuição
+var atribuição = 'tiago';
+
+// comparacao
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+// comparacao identica
+var comparacaoIdentica = '0'== 0;
+console.log(comparacaoIdentica);
+
+// adicao
+var adicao = 1 + 1;
+console.log(adicao);
+
+//subtracao
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+//multiplicacao
+/*var multiplicação
+var multiplicacao = 2 * 3;
+console.log(multiplicacao); */
+
+/*//divisao real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal); */
+
+/* //divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaointeira); */
+
+/*// potenciação
+var potenciacao = 2 **10;
+console.log(potenciacao)*/
+
+/*// maior que
+var maiorQue = 5 > 2;
+console.log(menorQue); */
+
+/* // menor
+var menorQue = 5 < 2;
+console.log (menorQue);*/
+
+/*// maior ou igual a
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual); */
+
+/* // menor ou igual
+var menorOuIgual = 5<=2;
+console.log(menorOuIgual)*/
+
+/*var e = true && true;
+console.log(e);*/
+
+/*var ou = false ||false;
+console.log(ou)*/
+
+/* var nao = !true;
+console.log(nao);*/
